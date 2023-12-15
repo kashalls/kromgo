59b90431972536306e926596f9f8b76e93cd37aa
Jordan Jones <jordpjones@gmail.com>
2023-12-15 10:38:59 -0800

    feat(go init): send out initial project

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # kubernetes-json-shields
+
+A simple go project that allows you to expose prometheus metrics "safely" to a public source. Better than exposing a grafana image rendering instance to the WWW.
+
+It allows you to define your own metric names and your own prometheus queries as long as they return a single value at the end. There is config support to allow you to format the response with strings before and after the value.
+
+You can use [shields.io](https://sheilds.io) and use either the [Dynamic JSON Badge](https://shields.io/badges/dynamic-json-badge) or the [Endpoint Badge](https://shields.io/badges/endpoint-badge) and add dynamic coloring with ranges you set.
+
+[Config Example](./config.yaml.example)
+
+## Performance
+
+Queries take around 5ms ~ 75ms to complete depending on how many breaks my prometheus server takes. This was running on my [home-cluster](https://github.com/kashalls/home-cluster) and runs 3 instances, so depending on the query YMMV.
+
+## Example Request
+
+### Endpoint Response
+
+This format is provided to support Shield.io's [Endpoint Badge](https://shields.io/badges/endpoint-badge) endpoint.
+
+`HTTP GET localhost:8080/query?format=endpoint&metric=node_cpu_usage`
+
+```json
+{
+    "color": "green",
+    "label": "node_cpu_usage",
+    "message": "17.5",
+    "schemaVersion": 1
+}
+```
+
+### Raw Response
+
+`HTTP GET localhost:8080/query?metric=node_cpu_usage`
+
+```json
+[
+    {
+        "metric": {},
+        "value": [
+            1702664619.78,
+            "17.5"
+        ]
+    }
+]
+```
+
+### 🤝 Gratitude and Thanks
+
+Thanks to all of the people at the [Home Operations](https://discord.gg/home-operations) Discord community. Be sure to check it out, its a blast!
---
+++ main.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+	"strconv"
+
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/client_golang/api"
+	"github.com/prometheus/client_golang/api/prometheus/v1"
+	"gopkg.in/yaml.v2"
+)
+
+type MetricColor struct {
+	Color string  `yaml:"color"`
+	Min   float64 `yaml:"min"`
+	Max   float64 `yaml:"max"`
+}
+
+type Metric struct {
+	Name   string        `yaml:"name"`
+	Query  string        `yaml:"query"`
+	Prefix string        `yaml:"prefix,omitempty"`
+	Suffix string        `yaml:"suffix,omitempty"`
+	Colors []MetricColor `yaml:"colors,omitempty"`
+}
+
+type Config struct {
+	Metrics []Metric `yaml:"metrics"`
+}
+
+type MetricResult struct {
+	Metric map[string]interface{} `json:"metric"`
+	Value  []interface{}          `json:"value"`
+}
+
+
+var configPath = "config.yaml.example" // Default config file path
+
+func main() {
+	// Check if a custom config file path is provided via command line argument
+	configPathFlag := flag.String("config", "", "Path to the YAML config file")
+	flag.Parse()
+
+	if *configPathFlag != "" {
+		configPath = *configPathFlag
+	}
+
+	// Load the YAML config file
+	config, err := loadConfig(configPath)
+	if err != nil {
+		fmt.Printf("Error loading config: %s\n", err)
+		os.Exit(1)
+	}
+
+	// Create a Prometheus API client
+	client, err := api.NewClient(api.Config{
+		Address: "https://prometheus.ok8.sh", // Replace with your Prometheus server URL
+	})
+	if err != nil {
+		fmt.Printf("Error creating Prometheus client: %s\n", err)
+		os.Exit(1)
+	}
+
+	// Create a Prometheus v1 API client
+	v1api := v1.NewAPI(client)
+
+	// Set up HTTP server
+	http.HandleFunc("/query", func(w http.ResponseWriter, r *http.Request) {
+		// Get the metric name from the query parameter
+		metricName := r.URL.Query().Get("metric")
+		responseFormat := r.URL.Query().Get("format")
+
+		// Find the corresponding metric configuration
+		var metric Metric
+		for _, configMetric := range config.Metrics {
+			if configMetric.Name == metricName {
+				metric = configMetric
+				fmt.Printf("Processing metric: %s with query %s\n", metric.Name, metric.Query)
+				break
+			}
+		}
+
+		// If metric not found, return an error
+		if metric.Query == "" {
+			fmt.Printf("Metric not found: %s\n", metricName)
+			http.Error(w, "Metric not found", http.StatusNotFound)
+			return
+		}
+
+		// Run the Prometheus query
+		result, _, err := v1api.Query(r.Context(), metric.Query, time.Now())
+		fmt.Printf("Query result: %s\n", result)
+		if err != nil {
+			fmt.Printf("Error executing query: %s\n", err)
+			http.Error(w, fmt.Sprintf("Error executing query: %s", err), http.StatusInternalServerError)
+			return
+		}
+
+		// Convert the result to JSON
+		jsonResult, err := json.Marshal(result)
+		fmt.Printf("non-json result: %s\n", result)
+		if err != nil {
+			fmt.Printf("Error converting result to JSON: %s\n", err)
+			http.Error(w, fmt.Sprintf("Error converting result to JSON: %s", err), http.StatusInternalServerError)
+			return
+		}
+
+		if responseFormat == "endpoint" {
+			resultValue := float64(result.(model.Vector)[0].Value)
+			color := getColor(metric.Colors, resultValue)
+			message := metric.Prefix + strconv.FormatFloat(resultValue, 'f', -1, 64) + metric.Suffix
+			data := map[string]interface{}{
+				"schemaVersion": 1,
+				"label": metricName,
+				"message": message,
+				"color": color,
+			}
+
+			// Convert the data to JSON
+			jsonData, err := json.Marshal(data)
+			if err != nil {
+				http.Error(w, "Error converting to JSON", http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jsonData)
+
+		} else {
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jsonResult)
+		}
+	})
+
+	// Determine the HTTP server port
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port
+	}
+
+	// Start the HTTP server
+	fmt.Printf("Server listening on :%s\n", port)
+	http.ListenAndServe(":"+port, nil)
+}
+
+// Load the YAML config file
+func loadConfig(path string) (*Config, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("error reading config file: %s", err)
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, fmt.Errorf("error unmarshalling YAML: %s", err)
+	}
+
+	return &config, nil
+}
+
+func getColor(colors []MetricColor, value float64) string {
+	for _, colorConfig := range colors {
+		if value >= colorConfig.Min && value <= colorConfig.Max {
+			return colorConfig.Color
+		}
+	}
+
+	return "unknown"
+}
